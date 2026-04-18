e846fefddf445cb68e02b0ac87c95590f217d147
linfang han <13703503153@163.com>
2026-04-18 22:24:47 +0800

    update

---
+++ SPCWB/Helpers/RedirectConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace SPCWB.Helpers
+{
+    public class RedirectConventer : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new Tuple<TabControl, int>((TabControl)values[0],(int)values[1]);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- SPCWB/ViewModels/Windows/MainWindowViewModel.cs
+++ SPCWB/ViewModels/Windows/MainWindowViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.ObjectModel;
-using Wpf.Ui.Controls;
+﻿using QRCoder;
+using System.Drawing;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace SPCWB.ViewModels.Windows
 {
@@ -7,5 +9,55 @@ namespace SPCWB.ViewModels.Windows
     {
         [ObservableProperty]
         private string _applicationTitle = "SPCWB";
+        [ObservableProperty]
+        private BitmapSource _qrSource;
+        private readonly string strCode = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+
+        [RelayCommand]
+        private void RedirectToPage(Tuple<TabControl, int> t)
+        {
+            t.Item1.SelectedIndex = t.Item2;
+        }
+        [RelayCommand]
+        private async Task GetQrCodeAsync()
+        {
+            QRCodeGenerator qrGenerator = new();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode("https://wasc.qzz.io/products/api/spcwb/qrcode.html?vc=" + await GetVerificationCodeAsync(), QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(100, Color.Black, Color.White, true);
+
+
+            IntPtr hBitmap = qrCodeImage.GetHbitmap();
+            // 将位图转换为 WPF 可以使用的 BitmapSource
+            QrSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                hBitmap,
+                IntPtr.Zero,
+                System.Windows.Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+        }
+        private async Task<string> GetVerificationCodeAsync()
+        {
+
+            var _charArray = strCode.ToCharArray();
+            var randomCode = "";
+            var temp = -1;
+            var rand = new Random(Guid.NewGuid().GetHashCode());
+            for (var i = 0; i < 8; i++)
+            {
+                if (temp != -1)
+                    rand = new Random(i * temp * (int)DateTime.Now.Ticks);
+                var t = rand.Next(strCode.Length - 1);
+                if (!string.IsNullOrWhiteSpace(randomCode))
+                    while (randomCode.ToLower().Contains(_charArray[t].ToString().ToLower()))
+                        t = rand.Next(strCode.Length - 1);
+                if (temp == t)
+                    return await GetVerificationCodeAsync();
+                temp = t;
+
+                randomCode += _charArray[t];
+            }
+
+            return randomCode;
+        }
     }
 }
